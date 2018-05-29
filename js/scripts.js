6e7104c092adeb27a9312bc32c66d678e2c9a627
Annie Shin and Reese Glasscock <annie@>
2018-05-29 14:37:49 -0700

    add day background and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,10 @@ $(document).ready(function() {
     $(".hidden").fadeIn();
   });
 });
+
+$(document).ready(function() {
+
+  $("button#day").click(function() {
+    $("body").addClass("day-background");
+  });
+});
