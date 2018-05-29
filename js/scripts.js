9f3c64fb80fedfeba9a7e264f7f5091113f66bdb
Annie Shin and Reese Glasscock <annie@>
2018-05-29 15:37:37 -0700

    update add highlight and border to paragraph upon click

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@ $(document).ready(function() {
     $(".hidden").fadeIn();
   });
 
+  $(".removeHighlight").click(function() {
+    $(".removeHighlight").addClass("highlight");
+  });
+
+  $("button#default").click(function() {
+    $("body").removeClass();
+    $("body").addClass("default-background");
+  });
+
   $("button#day").click(function() {
     $("body").removeClass();
     $("body").addClass("day-background");
